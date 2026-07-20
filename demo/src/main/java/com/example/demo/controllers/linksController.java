9934c635c5ab62cdd7022c1561358cd1e2a89014
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-07-20 21:03:07 +0000

    fix controllers

---
+++ demo/src/main/java/com/example/demo/controllers/linksController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Controller
+public class linksController {
+    private static final String BASE62 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @PostMapping("/shorten")
+    public String shorten(@RequestParam String param, Model model) {
+        StringBuilder shortCode = new StringBuilder(8);
+        
+        for (int i = 0; i < 8; i++) {
+            int index = ThreadLocalRandom.current().nextInt(BASE62.length());
+            shortCode.append(BASE62.charAt(index));
+        }
+        model.addAttribute("link", shortCode.toString());
+
+        return "index";
+    }
+}
