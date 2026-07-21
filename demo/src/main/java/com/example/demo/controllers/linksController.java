68addc27514f274815c3be4398fef6b87a7fe2ea
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-07-21 23:42:41 +0000

    add redirect logic

--- demo/src/main/java/com/example/demo/controllers/linksController.java
+++ demo/src/main/java/com/example/demo/controllers/linksController.java
@@ -6,6 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.net.URI;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
@@ -14,7 +22,7 @@ public class linksController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index.html";
     }
 
     @PostMapping("/shorten")
@@ -28,5 +36,11 @@ public class linksController {
         model.addAttribute("link", shortCode.toString());
 
         return "index";
+    }
+    @GetMapping("/redirect")
+    public String redirectToExternal() {
+
+        
+        return "redirect:https://wikipedia.org";
     }
 }
