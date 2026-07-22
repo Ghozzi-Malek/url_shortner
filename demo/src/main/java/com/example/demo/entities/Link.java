eb0dfcbf90264d78ba76ed3bf00e3f78a785087e
Male-g-tech <malek.ghozzi@insat.ucar.tn>
2026-07-22 11:10:19 +0000

    fix entities

--- demo/src/main/java/com/example/demo/entities/Link.java
+++ demo/src/main/java/com/example/demo/entities/Link.java
@@ -1,15 +1,32 @@
 package com.example.demo.entities;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @AllArgsConstructor
+@Entity
+@Table(name = "Link")
 public class Link {
-    private @Getter @Setter Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private @Getter @Setter Long id;
+
+    @Column(name = "long_form")
     private @Getter @Setter String long_form;
+    @Column(name = "short_form")
     private @Getter @Setter String short_form;
+    @Column(name = "prefix")
     private @Getter @Setter String prefix;
+    @Column(name = "duration")
     private @Getter @Setter String duration;
 
     
--- demo/src/main/java/com/example/demo/entities/User.java
+++ demo/src/main/java/com/example/demo/entities/User.java
@@ -1,13 +1,30 @@
 package com.example.demo.entities;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private @Getter @Setter Integer id;
+    @Column(name = "name")
     private @Getter @Setter String name;
+    @Column(name = "email")
     private @Getter @Setter String email;
+    @Column(name = "password")
     private @Getter @Setter String password;
 }
